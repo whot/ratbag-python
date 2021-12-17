06e05038cd7accd06a6c265419fb1f615bdc54c0
Peter Hutterer <peter.hutterer@who-t.net>
2021-12-17 11:33:02 +1000

    hid: set up an inverse mapping tablet too
    To enable mapping back from evdev to our enums

--- tests/test_hid.py
+++ tests/test_hid.py
@@ -8,5 +8,15 @@ def test_hid_evdev():
     assert ratbag.hid.Key.KEY_A.evdev == 30
     assert ratbag.hid.Key.KEY_RESERVED.evdev == 0
 
+    assert ratbag.hid.Key.from_evdev(1) == ratbag.hid.Key.KEY_ESCAPE
+    assert ratbag.hid.Key.from_evdev(30) == ratbag.hid.Key.KEY_A
+    assert ratbag.hid.Key.from_evdev(12345) is None
+
     assert ratbag.hid.ConsumerControl.CC_AC_DELETE.evdev == 111
     assert ratbag.hid.ConsumerControl.CC_AC_LOCK.evdev == 0
+
+    assert (
+        ratbag.hid.ConsumerControl.from_evdev(111)
+        == ratbag.hid.ConsumerControl.CC_AC_DELETE
+    )
+    assert ratbag.hid.ConsumerControl.from_evdev(12345) is None

73da4f963a3087f3432c606370a888d8bdec7f73
Peter Hutterer <peter.hutterer@who-t.net>
2021-12-23 14:46:30 +1000

    roccat: emit the resync signal if we had an error during commit

--- ratbag/drivers/roccat.py
+++ ratbag/drivers/roccat.py
@@ -657,6 +657,7 @@ class RoccatDevice(GObject.Object):
         def is_dirty(feature):
             return feature.dirty
 
+        needs_resync = False
         try:
             assert self.ratbag_device == ratbag_device
             logger.debug(f"Commiting to device {self.name}")
@@ -691,6 +692,7 @@ class RoccatDevice(GObject.Object):
                             self.write(ReportID.MACRO, macro_bytes)
                     except ratbag.ConfigError as e:
                         logger.error(f"{e}")
+                        needs_resync = True
 
                 keymap_bytes = bytes(profile.key_mapping)
                 logger.debug(f"Updating keymapping with {as_hex(keymap_bytes)}")
@@ -701,6 +703,10 @@ class RoccatDevice(GObject.Object):
         except Exception as e:
             logger.critical(f"::::::: ERROR: Exception during commit: {e}")
             traceback.print_exc()
+            needs_resync = True
+
+        if needs_resync:
+            ratbag_device.emit("resync")
 
     def write(self, report_id, bs):
         self.hidraw_device.hid_set_feature(int(report_id), bs)

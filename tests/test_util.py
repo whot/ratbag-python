544bde8024034076e898a13c5874d60eca242c85
Peter Hutterer <peter.hutterer@who-t.net>
2022-01-20 11:39:19 +1000

    util: drop the old attr to data converter functions
    The ratbag.parser.Parser does this better, so let's not maintain the
    same code twice.

--- tests/test_util.py
+++ tests/test_util.py
@@ -39,62 +39,6 @@ def test_device_info():
         assert info.report_descriptor is not None
 
 
-def test_attr_from_data():
-    class Foo(object):
-        pass
-
-    bs = bytes(range(16))
-    obj = Foo()
-    format = [
-        ("B", "zero"),
-        ("B", "first"),
-        (">H", "second"),
-        ("<H", "third"),
-    ]
-    offset = ratbag.util.attr_from_data(obj, format, bs, offset=0)
-    assert offset == 6
-    assert obj.zero == 0x0
-    assert obj.first == 0x1
-    assert obj.second == 0x0203
-    assert obj.third == 0x0504
-    reverse = ratbag.util.attr_to_data(obj, format)
-    assert reverse == bs[:offset]
-
-    bs = bytes(range(16))
-    obj = Foo()
-    format = [("BBB", "list")]
-    offset = ratbag.util.attr_from_data(obj, format, bs, offset=0)
-    assert offset == 3
-    assert obj.list == (0, 1, 2)
-    reverse = ratbag.util.attr_to_data(obj, format)
-    assert reverse == bs[:offset]
-
-    bs = bytes(range(16))
-    obj = Foo()
-    format = [("3*BBB", "list")]
-    offset = ratbag.util.attr_from_data(obj, format, bs, offset=0)
-    assert offset == 9
-    assert obj.list == [(0, 1, 2), (3, 4, 5), (6, 7, 8)]
-    reverse = ratbag.util.attr_to_data(obj, format)
-    assert reverse == bs[:offset]
-
-    bs = bytes(range(16))
-    obj = Foo()
-    format = [("H", "?"), ("3*BBB", "_")]
-    offset = ratbag.util.attr_from_data(obj, format, bs, offset=0)
-    assert offset == 11
-    reverse = ratbag.util.attr_to_data(obj, format)
-    assert reverse == bytes([0] * 11)
-
-    bs = bytes(range(16))
-    obj = Foo()
-    format = [("H", "something"), ("3*BBB", "other"), ("H", "map_me")]
-    offset = ratbag.util.attr_from_data(obj, format, bs, offset=0)
-    assert offset == 13
-    reverse = ratbag.util.attr_to_data(obj, format, maps={"map_me": lambda x: sum(x)})
-    assert reverse[-2] << 8 | reverse[-1] == sum(range(11))
-
-
 def test_add_to_sparse_tuple():
     t = (None,)
     t = ratbag.util.add_to_sparse_tuple(t, 3, "d")

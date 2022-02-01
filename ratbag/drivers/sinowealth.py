ee81b6cce3f07d9d9872a181ccd347c9ad5b8491
Yaroslav Chvanov <yaroslav.chvanov@gmail.com>
2022-02-01 03:11:53 +0300

    sinowealth: always enable XY_INDEPENDENT capability
    The byte in config data shows not whether it can be used, but whether
    it's activated right now.

--- ratbag/drivers/sinowealth.py
+++ ratbag/drivers/sinowealth.py
@@ -253,10 +253,7 @@ class SinowealthDevice:
             active=True,  # we only have one profile
         )
 
-        if config.independent_xy_resolution:
-            caps = [ratbag.Resolution.Capability.SEPARATE_XY_RESOLUTION]
-        else:
-            caps = []
+        caps = [ratbag.Resolution.Capability.SEPARATE_XY_RESOLUTION]
         # TODO: set the DPI range according to found sensor.
         # NOTE: 10200 is the biggest commonly supported resolution, it's used by PWM3327.
         dpi_list = tuple(range(100, 10200 + 1, 100))
